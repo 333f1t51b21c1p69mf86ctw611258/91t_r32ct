9626690c097ed1889550fc242d14e3cde98b134a
Tran Huu Nguyen <nguyenth@dasannetworks.com>
2016-12-22 11:40:27 +0700

    change gitignore and add hello2

---
+++ hello02/src/client.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<div>
+		<div>
+			<h1>Hello, world!</h1>
+			<p>test1</p>
+		</div>
+		<div>test2</div>
+	</div>,
+  	document.getElementById('root')
+);
