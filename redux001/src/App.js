802fdaf34555016a5cf954e56435650ae0335dd6
Nguyen FnF <nguyenth@dasanzhone.com>
2017-09-21 16:54:21 +0700

    Add checkbox function.

--- redux001/src/App.js
+++ redux001/src/App.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// <<<
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+const reducer = function (state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+    default:
+  }
+
+  return state;
+}
+
+const store = createStore(reducer);
+// >>>
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    store.subscribe(
+      () => this.setState(store.getState())
+    );
+  }
+
   render() {
+    const onClick = () => store.dispatch({ type: 'TOGGLE' });
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +44,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          <h1>To-dos</h1>
+          <div>
+            Learn Redux&nbsp;
+            <input type="checkbox" checked={!!this.state.checked} onClick={onClick} />
+          </div>
+          {
+            this.state.checked ? (<h2>Done!</h2>) : null
+          }
+        </div>
       </div>
     );
   }
